3291435b28fe7eecca452e58727fa8fbcbe15833
Adrian Serrano <adriansb3105@gmail.com>
2017-08-28 21:30:58 -0600

    SaleController ready

---
+++ WebServices/Controllers/SaleController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Business;
+using Domain;
+using Newtonsoft.Json;
+
+namespace WebServices.Controllers
+{
+    public class SaleController : ApiController
+    {
+
+        SaleBusiness saleBusiness = new SaleBusiness();
+
+        // GET: api/Sale
+        public List<Sale> Get()
+        {
+            return this.saleBusiness.GetSales();
+        }
+
+        // POST: api/Sale
+        public Sale Post([FromBody]Object value)
+        {
+            Sale sale = new Sale();
+            sale = JsonConvert.DeserializeObject<Sale>(value.ToString());
+
+            return this.saleBusiness.Insert(sale);
+        }
+
+    }
+}
